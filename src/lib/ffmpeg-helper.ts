e520a1b814ddda8d5c430ba59346aeea7b408d14
Mandy Schwarze <mandy2301@me.com>
2025-01-18 20:41:16 +0100

    fix: update ffmpeg command to include audio and cover mapping for temporary MP3 file creation

--- src/lib/ffmpeg-helper.ts
+++ src/lib/ffmpeg-helper.ts
@@ -14,7 +14,7 @@ export function addMetadataAndCoverToTempAndEncode({
   TEMPORARY_MP3_FILE,
 }: MetadataAndCover) {
   execSync(
-    `ffmpeg -f concat -safe 0 -i ${fileListPath} -i ${metadataFile} -i ${COVER_IMAGE_FILE} -map_metadata 1 -id3v2_version 3 -c:a libmp3lame -b:a 128k -metadata:s:v title="Cover" -metadata:s:v comment="Cover (front)" "${TEMPORARY_MP3_FILE}"`,
+    `ffmpeg -f concat -safe 0 -i ${fileListPath} -i ${metadataFile} -i ${COVER_IMAGE_FILE} -map_metadata 1 -map 0:a -map 2 -c copy -id3v2_version 3 -c:a libmp3lame -b:a 128k -metadata:s:v title="Cover" -metadata:s:v comment="Cover (front)" "${TEMPORARY_MP3_FILE}"`,
     { stdio: 'inherit' }
   )
 }
@@ -26,7 +26,7 @@ export function addMetadataAndCoverToTemp({
   TEMPORARY_MP3_FILE,
 }: MetadataAndCover) {
   execSync(
-    `ffmpeg -f concat -safe 0 -i ${fileListPath} -i ${metadataFile} -i ${COVER_IMAGE_FILE} -map_metadata 1 -id3v2_version 3 -metadata:s:v title="Cover" -metadata:s:v comment="Cover (front)" -c copy "${TEMPORARY_MP3_FILE}"`,
+    `ffmpeg -f concat -safe 0 -i ${fileListPath} -i ${metadataFile} -i ${COVER_IMAGE_FILE} -map_metadata 1 -map 0:a -map 2 -c copy -id3v2_version 3 -metadata:s:v title="Cover" -metadata:s:v comment="Cover (front)" -c copy "${TEMPORARY_MP3_FILE}"`,
     { stdio: 'inherit' }
   )
 }
